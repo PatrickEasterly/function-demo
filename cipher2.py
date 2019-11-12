56c486b3a162dbc5628778b3b67f8e21f212d24d
Patrick <pateast678@gmail.com>
2019-11-12 14:38:25 -0500

    added a convert letter function, used inside new convert sentence function

--- cipher2.py
+++ cipher2.py
@@ -1,4 +1,18 @@
 def convert_letter(letter, rotate_by=13):
     alphabet = list("abcdefghijklmnopqrstuvwxyz")
     # rotate_by is an optional argument
-    
+    position = alphabet.index(letter)
+    new_position = (position + rotate_by) % 26
+    new_letter = alphabet[new_position]
+
+    return new_letter
+
+def convert_sentence(sentence):
+    new_sentence = ''
+    for letter in sentence:
+        new_sentence += convert_letter(letter)
+    return new_sentence
+
+print(convert_sentence('hello'))
+# the_new_letter = convert_letter('a')
+# print(the_new_letter)
